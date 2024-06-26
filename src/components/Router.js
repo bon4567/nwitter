e71475d94e1ab1630b24c221f914d79398eccdb6
bon4567 <bon4567@gmail.com>
2024-06-26 21:50:12 +0900

    6/26

---
+++ src/components/Router.js
@@ -0,0 +1,22 @@
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import Auth from 'routes/Auth';
+import Home from 'routes/Home'
+
+const AppRouter = ({isLoggedIn}) => {
+  return (
+    <Router>
+      <Routes>
+        {
+          isLoggedIn ? (
+            <Route path='/' element={<Home />} />
+           ) : (
+            <Route path="/" element={<Auth />} />
+          )
+        }
+        <Route />
+      </Routes>
+    </Router>
+  )
+}
+
+export default AppRouter;
